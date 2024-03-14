664681adbf04f73e8471793a6cc925121056efd0
mufeng233 <2441260435@qq.com>
2024-03-14 14:35:53 +0800

    ✨ feat: 新增路由文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,22 @@
-/**
- * router/index.ts
- *
- * Automatic routes for `./src/pages/*.vue`
- */
-
-// Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from "vue-router/auto";
+import { setupLayouts } from "virtual:generated-layouts";
+import { useAuthStore } from "@/store/auth";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-})
+  extendRoutes: (routes) => setupLayouts(routes),
+});
 
-export default router
+router.afterEach((guard) => {
+  window.onbeforeunload = null;
+  const authStore = useAuthStore();
+  const isLogin = authStore.isLogin;
+  if (guard.meta.requireAuth && !isLogin) {
+    router.push("/login");
+  }
+  if (guard.meta.requireAuth && guard.path == "/login" && isLogin) {
+    router.push("/");
+  }
+});
+
+export default router;
